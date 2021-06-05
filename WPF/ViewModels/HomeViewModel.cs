4e77550c31a9506e2d17dc33748370b9f865c874
Anthony Rolland <anthony.rolland@etu.uca.fr>
2021-06-05 11:58:22 +0200

    Start to make form offre form

--- WPF/ViewModels/HomeViewModel.cs
+++ WPF/ViewModels/HomeViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using WPF.ViewModels.Common;
 
 namespace WPF.ViewModels
@@ -10,6 +11,7 @@ namespace WPF.ViewModels
     public class HomeViewModel : BaseViewModel
     {
         private ListOffresViewModel _listeOffreViewModel = null;
+        private RelayCommand _addOffre;
 
         public HomeViewModel()
         {
@@ -23,6 +25,21 @@ namespace WPF.ViewModels
         {
             get { return _listeOffreViewModel; }
             set { _listeOffreViewModel = value; }
+        }
+
+        public ICommand AddOffre
+        {
+            get
+            {
+                if (_addOffre == null)
+                    _addOffre = new RelayCommand(() => this.addOffre());
+                return _addOffre;
+            }
+        }
+
+        private void addOffre()
+        {
+            throw new NotImplementedException();
         }
 
     }
